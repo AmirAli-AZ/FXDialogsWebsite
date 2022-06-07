876081f0b49fbae1a6e47abaeb60e09834e50350
AmirAli-AZ <84965706+AmirAli-AZ@users.noreply.github.com>
2022-06-07 20:40:27 +0430

    update: javadocs

--- javadocs/package-search-index.js
+++ javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"FXDialogs","l":"com.amirali.fxdialogs"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"FXDialogs","l":"com.amirali.fxdialogs"},{"m":"FXDialogs","l":"com.amirali.fxdialogs.nodes"},{"m":"FXDialogs","l":"com.amirali.fxdialogs.notifications"}];updateSearchResults();
